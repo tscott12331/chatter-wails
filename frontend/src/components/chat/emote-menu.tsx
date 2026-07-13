88dd416b8bd5341bde34c2da408b28efd6820111
tscott12331 <t.scott12331@gmail.com>
2026-07-12 19:35:36 -0700

    feat: add fade and color transitions to emote popup menu

--- frontend/src/components/chat/emote-menu.tsx
+++ frontend/src/components/chat/emote-menu.tsx
@@ -32,14 +32,14 @@ export default function EmoteMenu({
     }, [emotes]);
     return (
         <div 
-            className={`${!open && 'invisible'} flex flex-col w-[calc(100%-30px)] h-75 border border-outline-1 rounded-xs m-3.5 absolute left-0 bottom-full bg-bg-2/80 backdrop-blur-xs p-1 z-600 *:select-none`}
+            className={`${!open && 'invisible opacity-0'} flex flex-col w-[calc(100%-30px)] h-75 border border-outline-1 rounded-xs m-3.5 absolute left-0 bottom-full bg-bg-2/80 backdrop-blur-xs p-1 z-600 *:select-none transition-all duration-75 ease-in-out transition-discrete`}
             ref={ref}
         >
             <div className="h-10 shrink-0 flex justify-around items-center gap-5 pb-1 ps-1 border-b border-b-outline-1">
                 {tabs.map((t, i) => 
                     <button 
                         key={t}
-                        className={`${i == tab ? 'bg-bg-3/50!' : 'bg-input-bg/50!'} cursor-pointer text-center grow rounded-xl border border-outline-2 px-2`}
+                        className={`${i == tab ? 'bg-bg-3/50!' : 'bg-input-bg/50!'} hover:bg-input-bg-3/50! cursor-pointer text-center grow rounded-xl border border-outline-2 px-2 transition-colors`}
                         onClick={() => setTab(i)}
                     >
                         {t}
@@ -56,7 +56,7 @@ export default function EmoteMenu({
                  key={emote.id}
                  >
                      <div
-                         className='flex justify-center items-center cursor-pointer w-10 h-10 p-0.5 rounded-xs opacity-90 hover:bg-bg-5'
+                         className='flex justify-center items-center cursor-pointer w-10 h-10 p-0.5 rounded-xs opacity-90 hover:bg-bg-5 transition-all transition-discrete'
                          onClick={() => handleEmoteSelect(emote)}
                      >
                          <img
