ea5afbd8e284e10742704b4f846816f7bfa8455c
tscott12331 <t.scott12331@gmail.com>
2026-07-19 20:57:55 -0700

    feat: add wails draggable var to titlebar to make it draggable

--- frontend/src/components/window/window-controls.tsx
+++ frontend/src/components/window/window-controls.tsx
@@ -11,7 +11,7 @@ export default function WindowControls() {
     }
 
     return (
-        <div className="relative h-2 bg-bg-1 border-b border-b-outline-1 hover:*:h-8 hover:*:opacity-100 z-5000">
+        <div className="titlebar relative h-2 bg-bg-1 border-b border-b-outline-1 hover:*:h-8 hover:*:opacity-100 z-5000">
             <div className="absolute opacity-0 bg-inherit inset-0 w-full flex justify-end items-center text-center text-lg transition-[height_opacity]">
                 <button onClick={Window.Minimise} className="w-8 h-8 rounded-none! border-none!">-</button>
                 <button onClick={handleMaxMinimise} className="w-8 h-8 rounded-none! border-none!">□</button>
