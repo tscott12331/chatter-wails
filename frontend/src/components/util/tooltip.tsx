a96c61f7fa07e0c0fd9720a2242de34b74b1a4e4
tscott12331 <t.scott12331@gmail.com>
2026-07-19 21:41:30 -0700

    fix: constrain tooltip popups to be within window horizontally

--- frontend/src/components/util/tooltip.tsx
+++ frontend/src/components/util/tooltip.tsx
@@ -17,25 +17,59 @@ interface ITooltipProps {
     data: TTooltipData;
 }
 
+const tooltipMaxImageWidth = 50;
+const tooltipMaxTextWidth = 300;
+
 export default function Tooltip({
     data,
 }: ITooltipProps) {
+    function determinePosition(posX: number, posY: number, type: TTooltipData["type"]) {
+        const windowWidth = window.innerWidth;
+
+        const posStyles = {};
+        const padding = (type == "text" 
+                        ? tooltipMaxTextWidth 
+                        : tooltipMaxImageWidth) / 2;
+
+        if(posX + padding > windowWidth) {
+            posStyles['right'] = padding;
+        } else if(posX < padding) {
+            posStyles['left'] = padding;
+        } else {
+            posStyles['left'] = posX;
+        }
+
+        posStyles['top'] = posY
+
+        return posStyles;
+    }
     return (
         <div
             className="absolute flex flex-col justify-center items-center z-10000 p-1 bg-bg-2/80 backdrop-blur-xs outline outline-outline-2/80 border border-outline-1 text-sm text-center wrap-break-word -translate-x-1/2 -translate-y-full rounded-[5%] shadow-[0_0_1px_1px] shadow-bg-9/50 italic"
             style={{
-                left: data.posX,
-                top: data.posY,
-                
+                ...determinePosition(data.posX, data.posY, data.type),
             }}
         >
         {data.type === "text"
-        ? <p className="max-w-[min(100%,300px)]">{data.text}</p>
+        ? <p 
+            style={{
+                maxWidth: `min(100%,${tooltipMaxImageWidth}px)`
+            }}
+        >{data.text}</p>
         : data.type === "image"
         ? <>
-        <img srcSet={data.imageSrcSet} className="max-w-25" />
+        <img 
+            srcSet={data.imageSrcSet}
+            style={{
+                maxWidth: `${tooltipMaxImageWidth}px`,
+            }}
+        />
         <div className="w-full border border-bg-9/50 mt-1.5 mb-0.5"></div>
-        <p className="max-w-full">{data.imageDesc}</p>
+        <p 
+            style={{
+                maxWidth: `${tooltipMaxImageWidth}px`,
+            }}
+        >{data.imageDesc}</p>
         </>
         : <></>
         }
