1a128ac416543a6ee3ac2f8dbb045b61925a2fb6
tscott12331 <t.scott12331@gmail.com>
2026-07-19 20:52:24 -0700

    feat: make window frameless and add window controls

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,32 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import Chatroom from "./pages/chatroom";
 import HomePage from "./pages/home";
 import NotFoundPage from "./pages/not-found";
-import ToastManager from "./components/util/toast/toast-manager";
-import { useContext } from "react";
-import { GlobalContext } from "./contexts/global-context";
-import { TooltipContext } from "./contexts/tooltip-context";
-import Tooltip from "./components/util/tooltip";
 
 interface IRouterProps {
 }
 
 export default function Router({
 }: IRouterProps) {
-    const { toast } = useContext(GlobalContext);
-    const { currentTooltip } = useContext(TooltipContext)
-
     return (
         <>
         <Routes>
             <Route path='/' element={<HomePage />}/>
-            <Route path='/chatroom/:channel' element={
-                <Chatroom />
-                }
+            <Route path='/chatroom/:channel' element={<Chatroom />}
             />
             <Route path='/:any' element={<NotFoundPage />} />
         </Routes>
-        <ToastManager toast={toast}/>
-        {currentTooltip && <Tooltip data={currentTooltip}/>}
         </>
     )
 }
