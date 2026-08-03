7fc54387300cf582031698ae51f4cc4b2610c447
tscott12331 <t.scott12331@gmail.com>
2026-08-02 19:33:38 -0700

    fix: fix search page header in place to prevent jittery movement on diffrent search result sizes

--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -64,8 +64,8 @@ export default function SearchPage() {
     }, []);
 
     return (
-        <div className="h-full p-5 flex flex-col items-center gap-5 scroller-y *:w-[min(672px,100%)] relative isolate">
-            <div className="w-full h-[max(15%,100px)] sticky -top-28 xs:-top-16 sm:-top-20 md:-top-24 flex flex-col justify-evenly items-center gap-5 z-1">
+        <div className="h-full p-5 flex flex-col items-center gap-5 scroller-y *:w-[min(672px,100%)] relative isolate scrollbar-gutter-both">
+            <div className="w-full shrink-0 h-37.5 sticky -top-28 xs:-top-24 sm:-top-25 md:-top-26 flex flex-col justify-evenly items-center gap-5 z-1">
                 <h1 className="font-extrabold text-5xl sm:text-6xl md:text-7xl text-center tracking-tight">Browse Channels</h1>
                 <form 
                     className="w-full p-1 flex items-center bg-input-bg/70 backdrop-blur-sm outline outline-input-outline rounded-md"
